3ee6fec10458a1862665507983354347793f464a
akshaywagh3 <akshayw3121@gmail.com>
2023-09-04 00:20:37 +0530

    navbar responsive property added

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,23 +1,30 @@
-import React from 'react';
+import React,{useRef} from 'react';
 import {NavLink} from 'react-router-dom';
+import { FaBars, FaTimes} from "react-icons/fa";
 import './Header.css';
 const Header = () => {
-  
-  
+  const navRef = useRef();
+
+  const showNavbar = () => {
+    navRef.current.classList.toggle("responsive");
+  };
   return (
     <header className='header'>
-        
-        
-        
-        <nav className='navbar'>
-            <h1>Akshay<span style={{'color':'red'}}>.</span></h1>
+      <h1>Akshay<span style={{'color':'red'}}>.</span></h1>
+        <nav className='navbar'ref={navRef}>
             <ul className='list'>
                 <NavLink to='/' className={({isActive})=>isActive ? 'active':undefined}>HOME</NavLink>
                 <li><NavLink to='/about' className={({isActive})=>isActive ? 'active':undefined}>ABOUT</NavLink></li>
                 <li><NavLink to='/project' className={({isActive})=>isActive ? 'active':undefined}>PROJECTS</NavLink></li>
                 <li><NavLink to='/contact' className={({isActive})=>isActive ? 'active':undefined}>CONTACT</NavLink></li>
             </ul>
+            <button className="nav-btn nav-close-btn" onClick={showNavbar}>
+          <FaTimes />
+            </button>
         </nav>
+        <button className="nav-btn" onClick={showNavbar}>
+        <FaBars />
+      </button>
         
     </header>
     
